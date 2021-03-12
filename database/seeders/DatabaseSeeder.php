49663fdc46e087587a7bfab1cdf839872647eede
Tara Matthew <taramatthew.93@gmail.com>
2021-03-12 16:38:35 +0000

    Add Feeback migration, model, factory, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(BrandSeeder::class);
         $this->call(SocialPlatformSeeder::class);
+        $this->call(FeedbackSeeder::class);
     }
 }
